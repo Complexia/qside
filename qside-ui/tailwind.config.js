62ece0a7f7b3aa97e4b675ce6d00dee33a62896b
Complexia <complexia701@gmail.com>
2023-10-12 16:49:24 +1100

    ui page commit

--- qside-ui/tailwind.config.js
+++ qside-ui/tailwind.config.js
@@ -1,18 +1,60 @@
+const { black } = require('tailwindcss/colors')
+const colors = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+
+  daisyui: {
+    themes: [
+      "synthwave",
+      "night",
+      "coffee",
+      "retro"
+      
+    ]
+  },
   theme: {
+
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      transitionProperty: {
+        'height': 'height',
+        'spacing': 'margin, padding',
+        'width': 'width'
       },
+
+      // colors: {
+      //   primaryBackgroundDark: colors.slate[700],
+      //   secondaryBackgroundDark: colors.slate[900],
+      //   primaryTextDark: colors.white,
+      //   darkBorder: colors.black,
+
+      //   primaryButtonWhite: colors.white,
+      //   primaryButtonDark: colors.black,
+
+      //   purpleButton: colors.purple[500],
+
+
+      //   primary: colors.black,
+      //   primaryAccent: colors.gray[600],
+      //   secondary: colors.gray[800],
+      //   secondaryAccent: colors.gray[200],
+      //   textPrimary: colors.black,
+      //   textSecondary: colors.white
+
+      // },
     },
   },
-  plugins: [],
+
+  plugins: [
+    require("tailwindcss-animate"),
+
+    require("daisyui"),
+    require("@tailwindcss/typography")
+  ],
 }
